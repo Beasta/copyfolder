8b17a295df2ee1ba41dad9cfeba82829c35f48d9
Beasta <barryblaha@gmail.com>
2017-05-17 16:26:44 -0700

    first commit!

---
+++ chromeit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if which live-server; then
+    echo 'live-server is installed'
+else
+    echo 'live-server is not  installed. installing'
+    npm i -g live-server
+fi
+
+if which browserify; then
+    echo 'browserify is installed'
+else
+    echo 'browserify is not  installed. installing'
+    npm i -g browserify
+fi
+
+if [ $# -eq 0 ]
+  then
+    echo "Give me a filename ... like this 'grump chromeit filename.js"
+fi
+
+for arg
+do
+  browserify $arg > bundle.js
+  echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body><script src="' bundle.js '"type="text/javascript"></script></body></html>' > _chromeit.html
+  trap "rm _chromeit.html && rm bundle.js" INT
+  echo "starting live-server which will monitor for file changes"
+  echo "use ctrl+c to kill the process"
+  live-server --entry-file="_chromeit.html"
+done
