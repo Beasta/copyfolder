eaf88d162123a50bee6d1ad6ed4f9c32dbe5f6cb
Beasta <barryblaha@gmail.com>
2017-05-18 12:20:18 -0700

    fix more issues

--- copyfolder.sh
+++ copyfolder.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ "$#" == 0 ] || [ "$#" -gt 1 ] ; then
+argc=$#
+if [ $argc -eq 0 -o $argc -eq 1 ]; then
 	echo "Must enter a folder to copy and its new name. Like this:"
 	echo "grump copyfolder originalFolderName copiedFolderName"
 else
-	cp -R $0 $1
+	cp -R $1 $2
 fi
